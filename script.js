08c33fb5dfbbda90fe207afc2e6828470c5f67e4
100日後にプログラミングで稼ぐ大学生｜ミワ_Miwa <121470211+Miwa-programming-and-AIweb@users.noreply.github.com>
2023-04-30 21:11:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,428 @@
+
+let divisionNumber=0;
+const statistics1=document.getElementById("argmin-solo");
+const statistics2=document.getElementById("argmin-solo-only-beta0");
+const statistics3=document.getElementById("argmin-solo-only-beta1");
+const statistics4=document.getElementById("mean");
+const statistics5=document.getElementById("variance");
+const statistics6=document.getElementById("covariance");
+const statistics7=document.getElementById("population-mean");
+const statistics8=document.getElementById("population-variance");
+const statistics9=document.getElementById("uncertainty");
+const statistics10=document.getElementById("x+uncertainty");
+
+statistics1.addEventListener("click",()=>{
+   divisionNumber=1;
+   document.getElementById("input-text-second").classList.remove("displayNone");
+   statistics1.style.color="white"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="#ae3ce3";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics2.addEventListener("click",()=>{
+   divisionNumber=2;
+   document.getElementById("input-text-second").classList.remove("displayNone");
+   statistics1.style.color="black"
+   statistics2.style.color="white"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="#ae3ce3";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics3.addEventListener("click",()=>{
+   divisionNumber=3;
+   document.getElementById("input-text-second").classList.remove("displayNone");
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="white"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="#ae3ce3";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics4.addEventListener("click",()=>{
+   divisionNumber=4;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="white"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="#ae3ce3";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics5.addEventListener("click",()=>{
+   divisionNumber=5;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="white"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="#ae3ce3";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics6.addEventListener("click",()=>{
+   divisionNumber=6;
+   document.getElementById("input-text-second").classList.remove("displayNone");
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="white"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="#ae3ce3";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics7.addEventListener("click",()=>{
+   divisionNumber=7;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="white"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="#ae3ce3";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics8.addEventListener("click",()=>{
+   divisionNumber=8;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="white"
+   statistics9.style.color="black"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="#ae3ce3";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics9.addEventListener("click",()=>{
+   divisionNumber=9;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="white"
+   statistics10.style.color="black"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="#ae3ce3";
+   statistics10.style.backgroundColor="rgb(236, 236, 236)";
+})
+statistics10.addEventListener("click",()=>{
+   divisionNumber=10;
+   statistics1.style.color="black"
+   statistics2.style.color="black"
+   statistics3.style.color="black"
+   statistics4.style.color="black"
+   statistics5.style.color="black"
+   statistics6.style.color="black"
+   statistics7.style.color="black"
+   statistics8.style.color="black"
+   statistics9.style.color="black"
+   statistics10.style.color="white"
+   statistics1.style.backgroundColor="rgb(236, 236, 236)";
+   statistics2.style.backgroundColor="rgb(236, 236, 236)";
+   statistics3.style.backgroundColor="rgb(236, 236, 236)";
+   statistics4.style.backgroundColor="rgb(236, 236, 236)";
+   statistics5.style.backgroundColor="rgb(236, 236, 236)";
+   statistics6.style.backgroundColor="rgb(236, 236, 236)";
+   statistics7.style.backgroundColor="rgb(236, 236, 236)";
+   statistics8.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics9.style.backgroundColor="rgb(236, 236, 236)";
+   statistics10.style.backgroundColor="#ae3ce3";
+})
+
+
+
+
+
+function getArr1Func(input1){    //1st valuesの配列を生成＆全角入力へのアラート
+   const inputTextArr1=input1.split(",");
+   const inputArr1=[];
+   for(let i=0;i<inputTextArr1.length;i++){
+      if( inputTextArr1.indexOf("０") != -1 || inputTextArr1.indexOf("１") != -1 || inputTextArr1.indexOf("２") != -1 || inputTextArr1.indexOf("３") != -1 || inputTextArr1.indexOf("４") != -1 || inputTextArr1.indexOf("５") != -1 || inputTextArr1.indexOf("６") != -1 || inputTextArr1.indexOf("７") != -1 || inputTextArr1.indexOf("８") != -1 || inputTextArr1.indexOf("９") != -1 ){
+         alert("半角で入力してください");
+         break;
+      }
+      let inputArr1element=parseFloat(inputTextArr1[i]);
+      inputArr1.push(inputArr1element);
+   }
+   return inputArr1;
+}
+
+function getArr2Func(input2){    //2nd valuesの配列を生成
+   const inputTextArr2=input2.split(",");
+   const inputArr2=[];
+   for(let i=0;i<inputTextArr2.length;i++){
+      let inputArr2element=parseFloat(inputTextArr2[i]);
+      inputArr2.push(inputArr2element);
+   }
+   return inputArr2;
+}
+
+
+function meanFunc(Arr){    //平均値を求める
+   let sum=0;
+   for(let i=0;i<Arr.length;i++){
+      sum+=Arr[i];
+   }
+   let mean=sum / Arr.length;
+   return mean;
+}
+
+function squareMeanFunc(Arr){    //2乗の平均値を求める
+   let squareSum=0;
+   for (let i=0;i<Arr.length;i++){
+      squareSum+=Arr[i] ** 2;
+   }
+   let squareMean=squareSum / Arr.length;
+   return squareMean;
+}
+
+function productMeanFunc(Arr1,Arr2){    //積の平均値を求める
+   let productSum=0;
+   for(let i=0;i<Arr1.length;i++){
+      productSum+=Arr1[i] * Arr2[i];
+   }
+   let productMean=productSum / Arr1.length;
+   return productMean;
+}
+
+
+
+const inputButton=document.getElementById("input-button");
+
+inputButton.addEventListener("click",()=>{
+   let Arr1=getArr1Func(document.getElementById("input-text").value);
+   const pointer=document.getElementById("memory-head-pointer");
+
+   switch(divisionNumber){
+      case 1:    //argmin-solo
+         let Arr2In1=getArr2Func(document.getElementById("input-text-second").value);
+         let meanX1=meanFunc(Arr1);
+         let meanY1=meanFunc(Arr2In1);
+         let varianceX1=squareMeanFunc(Arr1) - meanFunc(Arr1) ** 2;
+         let covariance1=productMeanFunc(Arr1,Arr2In1) - meanFunc(Arr1)*meanFunc(Arr2In1);
+         let beta_1In1=covariance1/varianceX1;
+         let beta_0In1=meanY1 - beta_1In1 * meanX1;
+         console.log(`(β0,β1) = (${beta_0In1}, ${beta_1In1})`);
+
+         let item1=document.createElement("p");
+         item1.textContent=`(β0,β1) = (${Math.round(beta_0In1*1000)/1000}, ${Math.round(beta_1In1*1000)/1000})`;
+         pointer.appendChild(item1);
+         break;
+
+      case 2:    //argmin-solo-only-beta0
+         let Arr2In2=getArr2Func(document.getElementById("input-text-second").value);
+         let beta_0In2=meanFunc(Arr2In2);
+         console.log(beta_0In2);
+
+         let item2=document.createElement("p");
+         item2.textContent=`β0 = ${Math.round(beta_0In2*1000)/1000}`;
+         pointer.appendChild(item2);
+         break;
+
+      case 3:    //argmin-solo-only-beta1
+         let Arr2In3=getArr2Func(document.getElementById("input-text-second").value);
+         let beta_1In3=productMeanFunc(Arr1,Arr2In3) / squareMeanFunc(Arr1);
+         console.log(beta_1In3);
+
+         let item3=document.createElement("p");
+         item3.textContent=`β1 = ${Math.round(beta_1In3*1000)/1000}`;
+         pointer.appendChild(item3);
+         break;
+
+      case 4:    //mean
+         let mean4=meanFunc(Arr1);
+         console.log(mean4);
+
+         let item4=document.createElement("p");
+         item4.textContent=`x- = ${Math.round(mean4*1000)/1000}`;
+         pointer.appendChild(item4);
+         break;
+
+      case 5:    //variance
+         let variance5=squareMeanFunc(Arr1) - meanFunc(Arr1) ** 2;
+         console.log(variance5);
+
+         let item5=document.createElement("p");
+         item5.textContent=`Sxx = ${Math.round(variance5*1000)/1000}`;
+         pointer.appendChild(item5);
+         break;
+
+      case 6:    //covariance
+         let Arr2In6=getArr2Func(document.getElementById("input-text-second").value);
+         let covariance6=productMeanFunc(Arr1,Arr2In6) - meanFunc(Arr1)*meanFunc(Arr2In6);
+         console.log(covariance6);
+
+         let item6=document.createElement("p");
+         item6.textContent=`Sxy = ${Math.round(covariance6*1000)/1000}`;
+         pointer.appendChild(item6);
+         break;
+
+      case 7:    //population-mean
+         let populationMean7=meanFunc(Arr1);
+         console.log(populationMean7);
+
+         let item7=document.createElement("p");
+         item7.textContent=`μ = ${Math.round(populationMean7*1000)/1000}`;
+         pointer.appendChild(item7);
+         break;
+      
+      case 8:    //population-variance
+         let variance8=squareMeanFunc(Arr1) - meanFunc(Arr1) ** 2;
+         let populationVariance8=(Arr1.length/(Arr1.length-1))*variance8;
+         console.log(populationVariance8);
+
+         let item8=document.createElement("p");
+         item8.textContent=`σ^2 = ${Math.round(populationVariance8*1000)/1000}`;
+         pointer.appendChild(item8);
+         break;
+
+      case 9:    //uncertainty
+         let variance9=squareMeanFunc(Arr1) - meanFunc(Arr1) ** 2;
+         let populationVariance9=(Arr1.length/(Arr1.length-1))*variance9;
+         let uncertainty9=populationVariance9 ** (1/2);
+         console.log(uncertainty9);
+
+         let item9=document.createElement("p");
+         item9.textContent=`σ = ${Math.round(uncertainty9*1000)/1000}`;
+         pointer.appendChild(item9);
+         break;
+
+      case 10:    //x±uncertainty
+         let variance10=squareMeanFunc(Arr1) - meanFunc(Arr1) ** 2;
+         let populationVariance10=(Arr1.length/(Arr1.length-1))*variance10;
+         let uncertainty10=populationVariance10 ** (1/2);
+         let populationMean10=meanFunc(Arr1);
+         let x_uncertainty10=populationMean10+" "+"±"+" "+uncertainty10;
+         console.log(x_uncertainty10);
+
+         let item10=document.createElement("p");
+         item10.textContent=`x±Δx = ${Math.round(populationMean10*1000)/1000} ± ${Math.round(uncertainty10*1000)/1000}`;
+         pointer.appendChild(item10);
+         break;
+
+
+         
+
+      default:
+         console.log("Error");
+   }
+})
+
